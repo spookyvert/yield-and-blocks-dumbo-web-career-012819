65c6fd0d4e785d6432a47da614b887ada67ac9f4
Shemar Dacosta <dshemar6@gmail.com>
2018-12-20 04:29:37 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -5,6 +5,7 @@ def hello_t(array)
     yield array[i]
     i = i +1
   end
+  arra7
 end
 
 # call your method here!
@@ -13,5 +14,5 @@ hello_t(["Tim","Tom","Jim"]) do |name|
     puts "Hi, #{name}"
   end
 
-  array
+
 end
